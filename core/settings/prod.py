e0f35d91f652209694a88b9060223ece8ad4f31f
AndreyK <kor313@yandex.ru>
2022-07-04 20:05:51 +0300

    change alowed hosts

--- core/settings/prod.py
+++ core/settings/prod.py
@@ -2,7 +2,7 @@ from .base import *
 
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['localhost', '192.168.0.26', '178.44.116.151']
 DJANGO_APPS = []
 DEV_APPS = []
 INSTALLED_APPS += PROJECT_APPS
