0ef498645c07818e3a0d4333856964553ee49ba3
Andrew KOr <kor313@yandex.ru>
2022-07-19 19:44:42 +0300

    е все хорошо смотреть TODO

--- system_statuses/views.py
+++ system_statuses/views.py
@@ -13,6 +13,7 @@ from system_statuses.utils import TestModuleApi
 def status(request):
     test_module = TestModuleApi()
     model_statuses = test_module.get_statuses()
+    # TODO сделать быстрый возврат таблицы с тестами, а результаты подтягивать по ходу выполнения в фоне Tasks
     return render(request,
                   template_name="system_statuses/status.html",
                   context={'statuses': model_statuses}
