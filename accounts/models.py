f7ab71351f5f25e28d7d151c442878d35ed92491
AndrewK <kor313@yandex.ru>
2022-07-27 16:43:12 +0300

    Add kerio description field

--- accounts/models.py
+++ accounts/models.py
@@ -1,19 +1,22 @@
 from django.db import models
 from django.contrib.auth import get_user_model
 from django.utils.translation import gettext_lazy as _
+
 User = get_user_model()
 
 
 # Create your models here.
 
 class IPAddress(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE, null=True, related_name='ip_address')
+    user = models.OneToOneField(User, on_delete=models.CASCADE, null=True, related_name='ip_address', default=None)
     ipaddress = models.CharField(max_length=15, verbose_name=_("IP address"), default="0.0.0.0")
     is_active = models.BooleanField(default=True, verbose_name=_("Is active"))
     in_kerio = models.BooleanField(default=False, verbose_name=_("Applied in Kerio"))
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
     kerio_id = models.CharField(max_length=5, null=True, blank=True, default=None, verbose_name=_("ID in KerioControl"))
+    kerio_description = models.CharField(max_length=200, blank=True, null=True,
+                                         verbose_name=_("Description in KerioControl "))
     kerio_group = models.ForeignKey("KerioGroup", on_delete=models.SET_NULL, null=True)
 
     class Meta:
