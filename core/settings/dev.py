1527bf7d8eab81c00e8e34f21d638965869d5a09
AndrewK <kor313@yandex.ru>
2022-08-12 09:36:55 +0300

    sentry settings from dev to prod file

--- core/settings/dev.py
+++ core/settings/dev.py
@@ -1,14 +1,10 @@
 from .base import *
-import sentry_sdk
-from sentry_sdk.integrations.django import DjangoIntegration
 
 DEBUG = True
 
 INTERNAL_IPS = [
     "127.0.0.1",
 ]
-
-# ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 
 DJANGO_APPS = []
 DEV_APPS = []
@@ -24,12 +20,3 @@ MIDDLEWARE += [
 TEMPLATES[0].update({"BACKEND": "django.template.backends.django.DjangoTemplates"})
 
 AUTH_PASSWORD_VALIDATORS = []
-
-sentry_sdk.init(
-    dsn=env.str("SENTRY_URL", "http://localhost:3333/"),
-    integrations=[
-        DjangoIntegration(),
-    ],
-    traces_sample_rate=1.0,
-    send_default_pii=True
-)
--- core/settings/prod.py
+++ core/settings/prod.py
@@ -1,3 +1,6 @@
+import sentry_sdk
+from sentry_sdk.integrations.django import DjangoIntegration
+
 from .base import *
 
 DEBUG = False
@@ -5,3 +8,12 @@ DEBUG = False
 DJANGO_APPS = []
 DEV_APPS = []
 INSTALLED_APPS += PROJECT_APPS
+
+sentry_sdk.init(
+    dsn=env.str("SENTRY_URL", "http://localhost:3333/"),
+    integrations=[
+        DjangoIntegration(),
+    ],
+    traces_sample_rate=1.0,
+    send_default_pii=True
+)
