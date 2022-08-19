5c83637a3dc422c6d02d1e8f57f759c077f594e7
AndrewK <kor313@yandex.ru>
2022-08-19 11:11:36 +0300

    refactoring

--- system_statuses/urls.py
+++ system_statuses/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import status
+from .views import status, status_table
 
 
 urlpatterns = [
     path('', status, name='status'),
+    path('table', status_table, name="status_table"),
 ]
--- system_statuses/views.py
+++ system_statuses/views.py
@@ -7,6 +7,7 @@ from django.shortcuts import render
 
 from django.conf import settings
 from django.contrib.auth.views import login_required
+from django.views.decorators.http import require_GET
 
 from system_statuses.utils import TestModuleApi
 from .tasks import start_tests
@@ -16,13 +17,13 @@ redis_instance = redis.StrictRedis(host=settings.REDIS_HOST,
 
 
 @login_required
-def status(request):
+@require_GET
+def status(request, partial=False):
     task_id = request.session.get('status_task_id')
     test_module = TestModuleApi()
     model_statuses = test_module.get_tests()
     if not task_id:
         task = start_tests.delay()
-        # task = start_tests()
         task_id = task.task_id
         request.session['status_task_id'] = task_id
     else:
@@ -36,9 +37,25 @@ def status(request):
     # if status_task.status == "SUCCESS":
     #     del request.session['status_task_id']
     #     redis_instance.flushdb()
+    context = {'statuses': model_statuses.values(),
+               'status_tasks': status_task.status}
+    if partial:
+        return context
 
     return render(request,
                   template_name="system_statuses/status.html",
-                  context={'statuses': model_statuses.values(),
-                           'status_tasks': status_task.status}
+                  context=context,
                   )
+
+
+@login_required
+@require_GET
+def status_table(request):
+    context = status(request, partial=True)
+
+    return render(
+        request,
+        template_name="system_statuses/includes/tbody.html",
+        context=context,
+        status=286 if context['status_tasks'] else None
+    )
